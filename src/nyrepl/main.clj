5df21b0aaf663d5311a3e5878430689153e05cfa
Oliver Caldwell <ollie@oli.me.uk>
2025-07-04 17:56:38 +0100

    Trying to get sci working and running in the native image - no success yet

--- src/nyrepl/main.clj
+++ src/nyrepl/main.clj
@@ -1,5 +1,7 @@
 (ns nyrepl.main
+  (:require [clojure.java.io :as io]
+            [sci.core :as sci])
   (:gen-class))
 
 (defn -main [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!" (sci/eval-string (slurp (io/resource "nyrepl/adapters/python.clj")))))
